393354547de681837e82250aadfbc3b4d61f6ec6
Jerry Waldorf <jwaldorf@ucsd.edu>
2021-02-09 00:25:48 -0800

    src

---
+++ src/jsdoc.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * @param {number} floors
+ * @param {number} bathrooms
+ * @param {number} rooms
+ * @param {string} street
+ */
+function house( floors, bathrooms, rooms, street ) {
+    let ret = `This house on ${street} street has ${floors} floors, ${bathrooms} bathrooms, and ${rooms} rooms.`;
+    return ret;
+}
